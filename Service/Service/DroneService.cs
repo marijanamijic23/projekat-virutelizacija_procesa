bf58070cbfef2a5208dd502e017863c4b801a528
M A R I J A N A <M A R I J A N A@DESKTOP-7EVSSFB>
2026-05-08 00:14:22 +0200

    Add project files.

---
+++ Service/Service/DroneService.cs
@@ -0,0 +1,27 @@
+﻿using Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Service
+{
+    public class DroneService : IDrone
+    {
+        public void endSession()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void pushSample(DroneSample sample)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void startSession(string meta)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Service/Service/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceModel;
+
+namespace Service
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ServiceHost host = new ServiceHost(typeof(DroneService));
+            host.Open();
+
+            Console.WriteLine("Service is running. Press any key to stop.");
+            Console.ReadKey();
+
+            host.Close();
+            Console.WriteLine("Service has stopped.");
+        }
+    }
+}
