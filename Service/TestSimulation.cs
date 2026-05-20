4be009bb33b51342b10375574d6bc8c4aded014b
M A R I J A N A <M A R I J A N A@DESKTOP-7EVSSFB>
2026-05-20 16:53:29 +0200

    Re-add Service project to repo

---
+++ Service/TestSimulation.cs
@@ -0,0 +1,36 @@
+﻿using Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Service
+{
+    public class TestSimulation
+    {
+        public void Test(string path)
+        {
+            using (CsvManipulation csv = new CsvManipulation(path))
+            {
+                try
+                {
+                    DroneService droneService = new DroneService();
+                    DroneSample sample = new DroneSample();
+                    sample.LinearAccelerationX = 13;
+                    sample.LinearAccelerationY = 12;
+                    sample.LinearAccelerationZ = 15;
+                    sample.WindAngle = 90;
+                    sample.WindSpeed = -77;
+                    sample.Time = 1.5;
+                    droneService.StartSession(path);
+                    droneService.PushSample(sample);
+                }
+                catch
+                {
+                    Console.WriteLine("Nagli prekid!");
+                }
+            }
+        }
+    }
+}
